080223d9201ed52b42a90f2ad596c82fa55d2fe6
Juan Severiano <juansoussev@gmail.com>
2022-11-03 21:36:20 -0300

    Novas Otimizações

--- teste01.py
+++ teste01.py
@@ -38,10 +38,11 @@ E a pedra amassa a tesoura
 """
 
 while True:
-    player = int(input('Você quer jogar?\nSim [1]\nNão [2]\n: '))
-    if player == 1:
-        i = int(input('Você ja sabe as regras o jogo?\nSim [1]\nNão [2]\n: '))
-        if i == 1:
+    j = input('Voce quer jogar? S/N\n')
+    if j == 's':
+        i = input('Você ja sabe as regras do jogo? S/N')
+        if (i == 'S') or (i == 'sim'):
+        
             while True:
                 print('Suas opcoes sao:\n{}'.format(l))
                 r = int(input('Qual opcao voce deseja? '))
@@ -50,56 +51,83 @@ while True:
                 print(f"Voce escolheu {d[r]} e o OCmputador escolheu {d[c]}")
                 if r == c:
                     print('EMPATE')
+                    
                 elif (r == 1) and (c == 2):
                     print('VOCÊ PERDEU\nPapel sufoca a Pedra')
+                    
                 elif (r == 1) and (c == 3):
                     print('VOCÊ VENCEU\nPedra esmaga Tesoura')
+                    
                 elif (r == 1) and (c == 4):
                     print('VOCÊ VENCEU\nPedra esmaga Lagarto')
+                    
                 elif (r == 1) and (c == 5):
                     print('EMPATE\nSpok valoriza a Pedra, todo o mundo sai ganhando')
+                    
+    
                 elif (r == 2) and (c == 3):
                     print('O Computador VENCEU\nTesoura corta papel')
+                    
                 elif (r == 2) and (c == 4):
                     print('O Computador VENCEU\nLagarto come Papel')
+                    
                 elif (r == 2) and (c == 5):
                     print('VOCÊ VENCEU\nPapel refuta Spock')
+                    
                 elif (r == 2) and (c == 1):
                     print('VOCÊ VENCEU\nPapel cobre a Pedra')
+                    
+    
                 elif (r == 3) and (c == 1):
                     print('VOCÊ VENCEU\nPedra amassa Tesoura')
+                    
                 elif (r == 3) and (c == 2):
                     print('VOCÊ VENCEU\nTesoura corta Papel')
+                    
                 elif (r == 3) and (c == 4):
                     print('VOCÊ VENCEU\nTesoura decapta Lagarto')
+                    
                 elif (r == 3) and (c == 5):
                     print('O Computador VENCEU\nSpock quebra a Tesoura')
+                    
+    
                 elif (r == 4) and (c == 1):
                     print('VOCÊ PERDEU\nPedra esmaga Lagarto')
+                    
                 elif (r == 4) and (c == 2):
                     print('VOCÊ VENCEU\nLagarto come Papel')
+                    
                 elif (r == 4) and (c == 3):
                     print('VOCÊ PERDEU\nTesoura decapta Lagarto')
+                    
                 elif (r == 4) and (c == 5):
                     print('VOCÊ VENCEU\nLagarto envenena Spock')
+                    
+    
                 elif (r == 5) and (c == 1):
                     print('EMPATE\nSpock valoriza a Pedra, todos saem ganhando')
+                    
                 elif (r == 5) and (c == 2):
                     print('VOCE PERDEU\nPapel refuta Spock')
+                    
                 elif (r == 5) and (c == 3):
                     print('VOCÊ VENCEU\nSpock quebra a Tesoura')
+                    
                 elif (r == 5) and (c == 4):
                     print('VOCÊ PERDEU\nLagarto envenena Spock')
                 p = input('Você quer continuar? S/N ')
                 if p == 'N':
                     break
                 sleep(3)
-        elif i == 2:
+                
+        elif i == 'N':
             print(regras)
-            sleep(2)
         else:
-            print('Digite um valor válido')
-    elif player == 2:
-        print('Obrigado bb, volte sempre e nao se esqueça, vc é incrivel')
+            print('Digite um valor valido')
+            
+    
+    elif (j == 'n') or (j == 'nao') or (j == 'não'):
+        print('pois valeu ai menó, volte sempre')
+    
     else:
-        print('Digite um valor valido')
+        print('digite um valor válido')
